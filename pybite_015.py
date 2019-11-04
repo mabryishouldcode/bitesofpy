6e0d2a2c0a88642e356391af6061017e03840f8d
James Mabry <james@jamesmabry.com>
2019-11-03 20:26:20 -0700

    Fixed last commit.

--- pybite_015.py
+++ pybite_015.py
@@ -1,12 +1,14 @@
-# ##############################
-# Bite 15. Enumerate 2 sequences
-# ##############################
+# ################################
+# Bite 015 - Enumerate 2 sequences
+# ################################
 
 #_HINTS_
 
 #_DESCRIPTION_
 
-# Iterate over the given names and countries lists, printing them prepending the number of the loop (starting at 1). Here is the output you need to deliver:
+# Iterate over the given names and countries lists, 
+#     printing them prepending the number of the loop (starting at 1). 
+#     Here is the output you need to deliver:
 
 # 1. Julian     Australia
 # 2. Bob        Spain
@@ -23,4 +25,40 @@
 # Ideally you use only one for loop, but that is not a requirement.
 
 # Good luck and keep calm and code in Python!
+
+names = 'Julian Bob PyBites Dante Martin Rodolfo'.split()
+countries = 'Australia Spain Global Argentina USA Mexico'.split()
+
+
+def enumerate_names_countries():
+    """Outputs:
+       1. Julian     Australia
+       2. Bob        Spain
+       3. PyBites    Global
+       4. Dante      Argentina
+       5. Martin     USA
+       6. Rodolfo    Mexico"""
+    pass
+
+
+
+# ###################################################################
+# _TESTS_
+# import pytest
+
+# from enumerate_data import enumerate_names_countries
+
+# expected_lines = ['1. Julian     Australia',
+#                   '2. Bob        Spain',
+#                   '3. PyBites    Global',
+#                   '4. Dante      Argentina',
+#                   '5. Martin     USA',
+#                   '6. Rodolfo    Mexico']
+
+
+# @pytest.mark.parametrize("line", expected_lines)
+# def test_enumerate_names_countries(capfd, line):
+#     enumerate_names_countries()
+#     output = capfd.readouterr()[0]
+#     assert line in output, f'{line} not in output'
 
