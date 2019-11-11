2ed75d9c4a7ad5766b1f34bd3a595d6b0f3dd0c3
James Mabry <james@jamesmabry.com>
2019-11-11 11:59:49 -0700

    Solved?

--- pybite_019.py
+++ pybite_019.py
@@ -12,21 +12,49 @@
 # Checkout the tests and datetime module for more info. Have fun!
 
 from datetime import datetime
+from datetime import timedelta
+
 
 NOW = datetime.now()
 
+# print(NOW > NOW - timedelta(seconds=1))
+
 class Promo:
-    
+
     def __init__(self, name):
         self.name = name
 
-#     def __str__(self, name):
-#         return '{}, {}'.format(self.name, )
+    def expired(self):
+        if NOW > NOW - timedelta(seconds=1):
+            return True
+        else:
+            return False
 
-#     def expired(self, name):
-#         if not NOW:
-#             return False
+# _TESTS_
+#####################################################################
+# def expired(name):
+#     pass
 
-# some_value = Promo('Twitter')
+# from datetime import timedelta
+# import inspect
+
+# from simple_property import Promo, NOW
+
+
+# def test_promo_expired():
+#     past_time = NOW - timedelta(seconds=3)
+#     twitter_promo = Promo('twitter', past_time)
+#     assert twitter_promo.expired
+
+
+# def test_promo_not_expired():
+#     future_date = NOW + timedelta(days=1)
+#     newsletter_promo = Promo('newsletter', future_date)
+#     assert not newsletter_promo.expired
+
+
+# def test_uses_property():
+#     assert 'property' in inspect.getsource(Promo)
+
 
 
